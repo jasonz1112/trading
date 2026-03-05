1f08237a4fd0a556ca8fc1f54f04086b3627cfbd
Jay <jasonz1112@gmail.com>
2026-03-04 18:09:42 -0800

    init

---
+++ application.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("start trading on ", time.Now().Format("2006-01-02"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jasonz1112/trading
+
+go 1.25.0
